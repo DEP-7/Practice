7d704e3e5729db39322d2d5df0646ff5f414f615
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-07 02:24:41 +0530

    push() and pop() method finished

--- Stack/src/dss/Stack.java
+++ Stack/src/dss/Stack.java
@@ -5,11 +5,29 @@ public class Stack {
     private int[] array;
 
     public void push(int number) {
-
+        if (array == null || array.length == 0) {
+            array = new int[1];
+            array[0] = number;
+        } else {
+            int[] temp = new int[array.length + 1];
+            for (int i = 0; i < array.length; i++) {
+                temp[i] = array[i];
+            }
+            temp[temp.length - 1] = number;
+            array = temp;
+        }
     }
 
     public void pop() {
-
+        if (array == null || array.length == 0) {
+            System.out.println("Array is empty. Can't pop");
+        } else {
+            int[] temp = new int[array.length - 1];
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = array[i];
+            }
+            array = temp;
+        }
     }
 
     public boolean empty() {
