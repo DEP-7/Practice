9b16d2a18e55624dd9451c30e2f360dca24fed6f
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-07 02:14:42 +0530

    Initialize all methods in Stack class

---
+++ Stack/src/dss/Stack.java
@@ -0,0 +1,38 @@
+package dss;
+
+public class Stack {
+
+    private int[] array;
+
+    public void push(int number) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public boolean empty() {
+        return false;
+    }
+
+    public int peek() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void print() {
+
+    }
+
+    public boolean contains(int number) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
