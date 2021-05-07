3a24012f9a8b822406ff5fa57b035ad9a533fef6
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-07 10:07:08 +0530

    Implements all methods in class Dequeue

--- Dequeue/src/dsdequeue/Dequeue.java
+++ Dequeue/src/dsdequeue/Dequeue.java
@@ -1,5 +1,32 @@
 package dsdequeue;
 
-public class Dequeue extends Queue{
+public class Dequeue extends Queue {
 
+    private int[] array;
+
+    public void enqueueRear(int number) {
+        if (array == null || array.length == 0) {
+            array = new int[1];
+            array[0] = number;
+        } else {
+            int[] temp = new int[array.length + 1];
+            temp[0] = number;
+            for (int i = 0; i < array.length; i++) {
+                temp[i + 1] = array[i];
+            }
+            array = temp;
+        }
+    }
+
+    public void dequeueFront() {
+        if (array == null || array.length == 0) {
+            System.out.println("Can't dequeue. Array is empty.");
+        } else {
+            int[] temp = new int[array.length - 1];
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = array[i];
+            }
+            array = temp;
+        }
+    }
 }
