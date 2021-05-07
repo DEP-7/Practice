73d27650dd59864804f7a3be6dd2906373cc2660
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-07 11:54:25 +0530

    add() methods implement and test finished

--- Dynamic Array/src/DynamicArrayTest.java
+++ Dynamic Array/src/DynamicArrayTest.java
@@ -1,2 +1,24 @@
+import dsdynamic.DynamicArray;
+
 public class DynamicArrayTest {
+    public static void main(String[] args) {
+        DynamicArray dynamicArray=new DynamicArray();
+
+        dynamicArray.add(10);
+        dynamicArray.print();
+        dynamicArray.add(20);
+        dynamicArray.print();
+        dynamicArray.add(30);
+        dynamicArray.print();
+
+        dynamicArray.add(5,50);
+        dynamicArray.print();
+        dynamicArray.add(2,25);
+        dynamicArray.print();
+        dynamicArray.add(0,5);
+        dynamicArray.print();
+        dynamicArray.add(4,29);
+        dynamicArray.print();
+
+    }
 }
--- Dynamic Array/src/dsdynamic/DynamicArray.java
+++ Dynamic Array/src/dsdynamic/DynamicArray.java
@@ -1,5 +1,94 @@
 package dsdynamic;
 
 public class DynamicArray {
+    protected int[] array;
 
+    public void add(int number) {
+        if (array == null || array.length == 0) {
+            array = new int[1];
+            array[0] = number;
+        } else {
+            int[] temp = new int[array.length + 1];
+            for (int i = 0; i < array.length; i++) {
+                temp[i] = array[i];
+            }
+            temp[temp.length - 1] = number;
+            array = temp;
+        }
+    }
+
+    public void add(int index, int number) {
+        if (array == null || index < 0 || array.length <= index) {
+            System.out.println("Index not exist");
+        } else {
+            int[] temp = new int[array.length + 1];
+            for (int i = 0; i < index; i++) {
+                temp[i] = array[i];
+            }
+            temp[index] = number;
+            for (int i = index+1; i < temp.length; i++) {
+                temp[i] = array[i-1];
+            }
+            array = temp;
+        }
+    }
+
+    public void remove(int index) {
+        if (array == null || array.length == 0) {
+            System.out.println("Can't dequeue. Array is empty.");
+        } else {
+            int[] temp = new int[array.length - 1];
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = array[i + 1];
+            }
+            array = temp;
+        }
+    }
+
+    public void get(int index) {
+        if (array == null || array.length == 0) {
+            System.out.println("Can't dequeue. Array is empty.");
+        } else {
+            int[] temp = new int[array.length - 1];
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = array[i + 1];
+            }
+            array = temp;
+        }
+    }
+
+    public boolean empty() {
+        return array == null || array.length == 0;
+    }
+
+    public void clear() {
+        array = null;
+    }
+
+    public void print() {
+        if (array == null || array.length == 0) {
+            System.out.println("[]");
+        } else {
+            System.out.print("[");
+            for (int number : array) {
+                System.out.print(number + ", ");
+            }
+            System.out.println("\b\b]");
+        }
+    }
+
+    public boolean contains(int number) {
+        if (array != null && array.length != 0) {
+            for (int numberInArray : array) {
+                if (number == numberInArray) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return array == null || array.length == 0 ? 0 : array.length;
+    }
 }
